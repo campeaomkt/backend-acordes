41446a7e383d6cc59d4a36980bfc007c928996e0
Eliab Campos <campeao.mkt.user@gmail.com>
2026-01-19 17:47:12 -0400

    fix render http port

--- server.js
+++ server.js
@@ -1,68 +1,20 @@
-const db = require("./database");
-
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
+// rota teste obrigatória
 app.get("/", (req, res) => {
- app.post("/register", (req, res) => {
-  const { email, password } = req.body;
-
-  db.run(
-    "INSERT INTO users (email, password, active) VALUES (?, ?, ?)",
-    [email, password, 0],
-    function (err) {
-      if (err) {
-        return res.status(400).json({ error: "Usuário já existe" });
-      }
-      res.json({ success: true });
-    }
-  );
+  res.send("Backend funcionando!");
 });
 
+// rota health para Render
+app.get("/healthz", (req, res) => {
+  res.status(200).send("OK");
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor rodando na porta " + PORT);
 });
-
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-
-  db.get(
-    "SELECT * FROM users WHERE email = ? AND password = ?",
-    [email, password],
-    (err, row) => {
-      if (!row) {
-        return res.status(401).json({ error: "Login inválido" });
-      }
-
-      if (row.active === 0) {
-        return res.status(403).json({ error: "Acesso bloqueado" });
-      }
-
-      res.json({ success: true });
-    }
-  );
-});
-
-app.post("/webhook-kiwify", (req, res) => {
-  const { email, status } = req.body;
-
-  if (status === "paid") {
-    db.run("UPDATE users SET active = 1 WHERE email = ?", [email]);
-  }
-
-  if (status === "refunded" || status === "canceled") {
-    db.run("UPDATE users SET active = 0 WHERE email = ?", [email]);
-  }
-
-  res.send("OK");
-});
-
